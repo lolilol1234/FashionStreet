cecd9492dbe4d01696a0e25399bb35de81539e69
Game_亮 <15517072173@163.com>
2018-03-30 12:00:52 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,70 @@
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./style/normalize.css";
+
+  body {
+    font-family: 'Microsoft YaHei';
+  }
+
+  ::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    background: transparent;
+    opacity: .8;
+  }
+
+  ::-webkit-scrollbar-button {
+    width: 0;
+    height: 0;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: rgba(54, 142, 224, 0.5);
+    /*border: thin solid #fa9000;*/
+    border-radius: 8px;
+    -webkit-transition: background .25s linear;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: #F0F0F0;
+    border: 1px solid #F0F0F0;
+    box-shadow: none;
+    border-radius: 0;
+  }
+
+  .el-dialog__header {
+    background: #368ee0;
+    .el-dialog__title {
+      font-size: 16px;
+      color: #fff;
+    }
+    .el-dialog__headerbtn .el-dialog__close {
+      color: #fff;
+    }
+  }
+
+  @media (max-width: 1366px) {
+    .operate-dialog-form {
+      padding: 10px;
+      max-height: 380px;
+      overflow-y: auto;
+      overflow-x: hidden;
+    }
+  }
+
+  @media (max-width: 1920px) and (min-width: 1367px) {
+    .operate-dialog-form {
+      padding: 10px;
+      max-height: 500px;
+      overflow-y: auto;
+      overflow-x: hidden;
+    }
+  }
 </style>
